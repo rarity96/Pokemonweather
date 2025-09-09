549978f4140873b7d38f84fe80e45ef4ca514244
Radosław Strugała <struradek196@gmail.com>
2025-09-09 14:14:02 +0200

    list of users

--- pages/3.Statistic.py
+++ pages/3.Statistic.py
@@ -1,14 +1,6 @@
 from mainapp import st, sqlite_connect
 import  pandas as pd
-import sqlite3
 st.write("Do przegladania statystyk, wyświetlania wykresów i danych.")
-
-@st.cache_resource
-def sqlite_connect():
-    con = sqlite3.connect('pokeweather.db', check_same_thread=False)
-    con.execute("PRAGMA foreign_keys = ON")
-    con.execute("PRAGMA journal_mode = WAL")
-    return con
 
 con = sqlite_connect()
 c = con.cursor()
